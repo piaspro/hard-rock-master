9c1e79f2d4c2c3003c95fe74e22a685aaa38977d
piaspro <golamrahman09@gmail.com>
2020-08-17 11:47:24 +0600

    search done

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+// Display none
+const lyricsArea = document.getElementById("lyricsArea");
+lyricsArea.style.display = "none";
 // Active search area
 const searchArea = document.getElementById("searchArea");
 searchArea.addEventListener('keypress', function search(press) {
@@ -13,13 +16,28 @@ searchBtn.addEventListener('click', function () {
 })
 // Get data from api
 function getResults(value) {
-    fetch(`https://api.lyrics.ovh/suggest/summer`)
+    fetch(`https://api.lyrics.ovh/suggest/${value}`)
     .then(response => response.json())
-    .then(data => console.log(data))
+    .then(songs => displayResult(songs))
 }
-// Display none
-const lyricsArea = document.getElementById("lyricsArea");
-lyricsArea.style.display = "none"; 
-const searchResult = document.getElementById("searchResult");
-searchResult.style.display = "none"; 
+// Display result function
+function displayResult(songs) {
+    const allSongs = songs.data;
+    const searchResult = document.getElementById("searchResult");
+    for (let i = 0; i < 10; i++) {
+        const title = allSongs[i].title;
+        const artist = allSongs[i].album.title;
+        searchResult.innerHTML += `<div class="single-result row align-items-center my-3 p-3">
+                                    <div class="col-md-9">
+                                        <h3 class="lyrics-name">${title}</h3>
+                                        <p class="author lead">Album by <span>${artist}</span></p>
+                                    </div>
+                                    <div class="col-md-3 text-md-right text-center">
+                                        <button id="getLyrics" class="btn btn-success">Get Lyrics</button>
+                                    </div>
+                                </div>`
+    }
+    
+}
+// get lyrics
 
